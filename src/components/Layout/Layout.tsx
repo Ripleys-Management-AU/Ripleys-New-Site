8cb1c17af5dc457d519bb8f74a525e4434283231
wendy <s4594789@student.uq.edu.au>
2024-06-27 19:35:15 +1000

    fix build issues

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,11 @@
 import Navbar from "@/components/Navbar";
 import React from "react";
 
-const Layout: React.FC = ({ children }) => {
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<Props> = ({ children }) => {
   return (
     <div>
       <Navbar />
