2a4fa99290bf3e418fdf2167bc491f8273dd7858
wendy <s4594789@student.uq.edu.au>
2024-06-27 12:30:20 +1000

    init

---
+++ src/pages/_document.tsx
@@ -0,0 +1,63 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+class MyDocument extends Document {
+  render() {
+    return (
+        <Html lang="en" className="">
+          <Head>
+            <link
+                rel="apple-touch-icon"
+                sizes="76x76"
+                href="/favicons/favicon.ico"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicons/favicon.ico"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicons/favicon.ico"
+            />
+            <link rel="manifest" href="/favicons/site.webmanifest" />
+            <link
+                rel="mask-icon"
+                href="/favicons/favicon.ico"
+                color="#5bbad5"
+            />
+
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link
+                rel="preconnect"
+                href="https://fonts.gstatic.com"
+                crossOrigin="true"
+            />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Geo:ital@0;1&display=swap"
+                rel="stylesheet"
+            />
+
+            <meta name="msapplication-TileColor" content="#000000" />
+            <meta
+                name="theme-color"
+                media="(prefers-color-scheme: light)"
+                content="#fff"
+            />
+            <meta
+                name="theme-color"
+                media="(prefers-color-scheme: dark)"
+                content="#000"
+            />
+          </Head>
+          <body className="bg-white">
+          <Main />
+          <NextScript />
+          </body>
+        </Html>
+    );
+  }
+}
+
+export default MyDocument;
