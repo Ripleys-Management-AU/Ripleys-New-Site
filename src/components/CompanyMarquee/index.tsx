c09ab578e6a2361e8cb7b0f413948004cde7ecfc
wendy <s4594789@student.uq.edu.au>
2024-06-28 10:19:52 +1000

    add title for marquee section

--- src/components/CompanyMarquee/index.tsx
+++ src/components/CompanyMarquee/index.tsx
@@ -3,14 +3,17 @@ import { companyLogos } from "@/data/companyLogos";
 
 const CompanyMarquee = () => {
   return (
-    <div>
+    <div className="text-center my-4">
+      <h1 className="uppercase text-4xl font-semibold text-gray-300 mb-8">
+        Who We&apos;Re Working With
+      </h1>
       <Marquee>
         {companyLogos.map((item) => (
           <img
             key={item.id}
             src={item.image}
             alt={item.desc}
-            className="h-[100px] mr-16"
+            className="h-[80px] mr-16"
           />
         ))}
       </Marquee>
