8b08681d3313ff3914e694a0e186c773ea2f9885
wendy <s4594789@student.uq.edu.au>
2024-06-29 12:50:35 +1000

    change talent view mobile margin top

--- src/components/TalentView/TalentView.tsx
+++ src/components/TalentView/TalentView.tsx
@@ -10,7 +10,7 @@ interface props {
 const TalentView: React.FC<props> = ({ talents }) => {
   return (
     <div className="w-full flex justify-center">
-      <div className="min-h-screen mt-40 w-4/5">
+      <div className="min-h-screen mt-20 lg:mt-40 w-4/5">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 place-items-center w-full">
           {talents?.map((talent) => (
             <div key={talent.id} className="w-full">
