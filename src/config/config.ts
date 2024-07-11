0d819eaf0efd25b2f5a1581046c703898c4de1dd
wendy <s4594789@student.uq.edu.au>
2024-07-11 13:27:37 +1000

    change email sender address

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,8 @@ const config = {
   talentPerPage: Number(process.env.NEXT_PUBLIC_TALENT_PER_PAGE) || 9,
   uploadPrefix: process.env.NEXT_PUBLIC_UPLOADS_PREFIX || "/uploads",
   smtp2goApiKey: process.env.SMTP2GO_API_KEY || "",
-  emailSenderAddress: process.env.EMAIL_SENDER_ADDRESS || "admin@akiraspace.io",
+  emailSenderAddress:
+    process.env.EMAIL_SENDER_ADDRESS || "noreply@ripleys.site",
   ripleysAdminAddress:
     process.env.RIPLEYS_ADMIN_ADDRESS || "rm@ripleysmanagement.com.au",
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY || "",
